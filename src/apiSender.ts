3c12292b21d603fd968ea682e965bb874bc17ba8
HenryD5 <david_yamunaque_5@outlook.com>
2022-12-15 00:17:54 -0500

    add token for api

--- src/apiSender.ts
+++ src/apiSender.ts
@@ -3,16 +3,21 @@ import type { Data } from './models/types'
 
 class ApiSender {
   private url: string;
+  private token: string;
 
-  constructor(url: string) {
+  constructor(url: string, token: string) {
     this.url = url;
+    this.token = token;
   }
 
   public async sendData(data: Data | any) {
     try {
+      const config = {
+        headers: { Authorization: `Bearer ${this.token}` }
+      };
       console.log('url ...', this.url)
       console.log('data ...', data)
-      const response = await axios.post(this.url, data);
+      const response = await axios.post(this.url, data, config);
       return response.data;
     } catch (error) {
       throw error;
